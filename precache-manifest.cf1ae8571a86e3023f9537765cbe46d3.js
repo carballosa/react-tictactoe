58dcb968d19dc77178f51ef6031797ef1c0bc133
Jose Carballosa <jose.carballosa@gmail.com>
2019-01-30 12:33:33 +0100

    Updates

---
+++ precache-manifest.cf1ae8571a86e3023f9537765cbe46d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe3c286524cc5894d260",
+    "url": "/tictactoe-react/static/js/runtime~main.fe3c2865.js"
+  },
+  {
+    "revision": "0f8b25284200e86fa712",
+    "url": "/tictactoe-react/static/js/main.0f8b2528.chunk.js"
+  },
+  {
+    "revision": "b3fbd30101660ab66b22",
+    "url": "/tictactoe-react/static/js/1.b3fbd301.chunk.js"
+  },
+  {
+    "revision": "0f8b25284200e86fa712",
+    "url": "/tictactoe-react/static/css/main.ddadc50f.chunk.css"
+  },
+  {
+    "revision": "23f64ded0ea2c1feb39ed947ea7475db",
+    "url": "/tictactoe-react/index.html"
+  }
+];
